18599fa7b9f4a77b150dcc08dfb2d009538165b0
Sadham Hussian M <sadhamhussian2020@gmail.com>
2026-03-28 20:53:11 +0530

    patch: add inital files

--- Formula/recall.rb
+++ Formula/recall.rb
@@ -1,8 +1,8 @@
 class Recall < Formula
   desc "Intelligent CLI history with semantic search"
   homepage "https://github.com/Sadham-Hussian/recall"
-  url "https://github.com/Sadham-Hussian/recall/releases/download/v0.1.2/recall_0.1.2_darwin_arm64.tar.gz"
-  sha256 "a4a35e474b09ab3cd612dbc23f1abbcddc56d426da75e862ce5776f1ac6932d6"
+  url "https://github.com/Sadham-Hussian/recall/releases/download/v0.1.6/recall_0.1.6_darwin_arm64.tar.gz"
+  sha256 "a347dad011d5a8cdb1977dc9d764880c5df843f5b58e79ca9b86f827469774c3"
   license "MIT"
 
   def install

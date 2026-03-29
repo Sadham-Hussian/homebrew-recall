b20fa7d7efafede3c7ddb634142f836f3c60a92a
Sadham Hussian M <sadhamhussian2020@gmail.com>
2026-03-29 16:14:17 +0530

    patch: add the release hashes

--- Formula/recall.rb
+++ Formula/recall.rb
@@ -6,10 +6,10 @@ class Recall < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Sadham-Hussian/recall/releases/download/v1.0.0/recall_darwin_arm64.tar.gz"
-      sha256 "fb0812ea1d1d205c71b78dbb461c6dd82163cdecc09eb76ee79dbbf57b25916b"
+      sha256 "4dbed3c8cc85e2b05ea3c556bf8b4b230b6fb5f34d2827369bd36017adbaac6c"
     else
       url "https://github.com/Sadham-Hussian/recall/releases/download/v1.0.0/recall_darwin_amd64.tar.gz"
-      sha256 "8948eb52c4522d775b0bd054a7533b8f54051b494472ff1ecf7f53ea04c2b7c1"
+      sha256 "28f7b0b077895f6f2c7ca85ea49ace4177ba7066f68106eec9c97dcf935d99a0"
     end
   end
 

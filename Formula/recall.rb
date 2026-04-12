039bd7f2edee2a3b7e9d97895c6e2fc1b532d880
Sadham Hussian M <sadhamhussian2020@gmail.com>
2026-04-12 19:46:33 +0530

    patch: update checksum ffor 1.0.1

--- Formula/recall.rb
+++ Formula/recall.rb
@@ -5,10 +5,10 @@ class Recall < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Sadham-Hussian/recall/releases/download/v1.0.0/recall_darwin_arm64.tar.gz"
+      url "https://github.com/Sadham-Hussian/recall/releases/download/v1.0.1/recall_darwin_arm64.tar.gz"
       sha256 "65f6d6aa9e26c958a4837acea52c95168beaf63e49b218f19ac2e8e97aa9ddfa"
     else
-      url "https://github.com/Sadham-Hussian/recall/releases/download/v1.0.0/recall_darwin_amd64.tar.gz"
+      url "https://github.com/Sadham-Hussian/recall/releases/download/v1.0.1/recall_darwin_amd64.tar.gz"
       sha256 "9727d3ffe163ad799471818045b3b780f4ca5745ce7eaccaac5def9a6418c8a1"
     end
   end
